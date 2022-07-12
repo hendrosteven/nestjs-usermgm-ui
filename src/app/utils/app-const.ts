81158c4c4fbe02398711a3786638be02568b512f
Hendro Steven <hendro.steven@gmail.com>
2022-07-13 02:15:19 +0700

    update url

--- src/app/utils/app-const.ts
+++ src/app/utils/app-const.ts
@@ -1,2 +1,2 @@
-//export const BASE_URL: string = 'https://user-app-api.herokuapp.com';
-export const BASE_URL: string = 'http://localhost:2525';
+export const BASE_URL: string = 'https://user-app-api.herokuapp.com';
+//export const BASE_URL: string = 'http://localhost:2525';
