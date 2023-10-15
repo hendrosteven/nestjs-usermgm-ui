b89887a8bf98d6b0ef88f2f0ebb6ed00d3e1ed14
Hendro Steven Tampake <hendro.steven@gmail.com>
2023-10-16 00:05:09 +0700

    Update app-const.ts

--- src/app/utils/app-const.ts
+++ src/app/utils/app-const.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = 'https://user-app-api.herokuapp.com';
+export const BASE_URL: string = 'https://user-app-api-da3f078983d0.herokuapp.com';
 //export const BASE_URL: string = 'http://localhost:2525';
 export const OAUTH_GOOGLE_HEROKU: string = '841779677180-09s4e6e0sqk52viq5j8hukb9cnclv9nb.apps.googleusercontent.com';
 export const OAUTH_GOOGLE_LOCAL: string = '841779677180-9d3babbiqpbqrhavvndflkhm3gu6c36h.apps.googleusercontent.com';
